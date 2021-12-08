35601910ffc7b560498a5e80a95a132b6723765a
Fitchu <fedor7495@gmail.com>
2021-12-08 22:24:08 +0500

    Task-1: Создал функцию, которая создает HTML структуру для отображения персонажа

--- main.js
+++ main.js
@@ -17,3 +17,31 @@ const player2 = {
     console.log(name + "Fight...");
   },
 };
+
+function createPlayer() {
+  const $player1 = document.createElement("div");
+  $player1.classList.add("player1");
+
+  const $progressbar = document.createElement("div");
+  $progressbar.classList.add("progressbar");
+
+  const $life = document.createElement("div");
+  $life.classList.add("life");
+
+  const $name = document.createElement("div");
+  $name.classList.add("name");
+  $name.innerText = "Китана";
+
+  $progressbar.appendChild($life);
+  $progressbar.appendChild($name);
+
+  const $character = document.createElement("div");
+  $character.classList.add("character");
+
+  const $img = document.createElement("img");
+  $img.src = "http://reactmarathon-api.herokuapp.com/assets/kitana.gif";
+  $character.appendChild($img);
+
+  $player1.appendChild($progressbar);
+  $player1.appendChild($character);
+}
