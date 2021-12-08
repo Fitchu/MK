65f9c5e7e773e182429026bece990615a7a3fdb9
Fitchu <fedor7495@gmail.com>
2021-12-08 21:57:57 +0500

    Task-0: Создал два объекта игроков с различными характеристиками.

---
+++ main.js
@@ -0,0 +1,19 @@
+const player1 = {
+  name: "Китана",
+  hp: 100,
+  img: "",
+  weapon: ["Боевые вееры"],
+  attack: function (name) {
+    console.log(name + "Fight...");
+  },
+};
+
+const player2 = {
+  name: "Соня",
+  hp: 100,
+  img: "",
+  weapon: ["Бамбуковые палки"],
+  attack: function (name) {
+    console.log(name + "Fight...");
+  },
+};
